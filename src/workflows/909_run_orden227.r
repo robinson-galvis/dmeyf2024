aed152c4e269e206d9c4d199e0280f54ed2ba848
robinson-galvis <robins.gama@gmail.com>
2024-11-29 18:54:26 -0300

    wf_semillero_rf_a25_cn_r1_d0_sem881269_comp3_usmp_02

--- src/workflows/909_run_orden227.r
+++ src/workflows/909_run_orden227.r
@@ -1,7 +1,7 @@
 require("rlang")
 
 # workflow que voy a correr
-PARAM <- "src/workflows/990_wf_semillero_rf_a25_sem881269_comp3_lag3.r"
+PARAM <- "src/workflows/990_wf_semillero_rf_a25_cn_r1_d0_sem881269_comp3_usmp_02.r"
 
 envg <- env()
 
