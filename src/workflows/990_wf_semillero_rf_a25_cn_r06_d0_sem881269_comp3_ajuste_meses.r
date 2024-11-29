4ea1d4ca2fe1fc7a619771774fe565ce208a2e14
robinson-galvis <robins.gama@gmail.com>
2024-11-29 11:07:09 -0300

    wf_semillero_rf_a25_cn_r06_d0_sem881269_comp3_ajuste_meses - Kaggle

--- src/workflows/990_wf_semillero_rf_a25_cn_r06_d0_sem881269_comp3_ajuste_meses.r
+++ src/workflows/990_wf_semillero_rf_a25_cn_r06_d0_sem881269_comp3_ajuste_meses.r
@@ -466,7 +466,7 @@ KA_evaluate_kaggle_semillerio <- function( pinputexps )
   param_local$envios_desde <- 9000L
   param_local$envios_hasta <- 13050L
   param_local$envios_salto <-   500L
-  param_local$competition <- "dm-ey-f-2024-segunda"
+  param_local$competition <- "dm-ey-f-2024-tercera"
 
   return( exp_correr_script( param_local ) ) # linea fija
 }
